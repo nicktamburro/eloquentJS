20a8689defba4d658c00daa37b0d251d8d5c2b9f
nicktamburro <nicholastamburro@gmail.com>
2018-03-17 22:03:15 -0700

    fixed recursion solution, no more infinite loop

--- 4-recursion.js
+++ 4-recursion.js
@@ -1,20 +1,19 @@
 //EXERCISE: write a recursive function that checks for even or odd, without using %...
 //
-//so there has to be a mini-function that just subtracts by 2 until the number = 0 or 1... 
-//it just keeps getting called...
 
-let number = 37;
+
 
 function isEven(number){
     if (number === 0){
         console.log("true");
+        return true;
     }if (number === 1){
         console.log("false");
+        return "false";
     }else{
         number = number - 2;
-        console.log("number");
         isEven(number);
     }
 }
 
-isEven(number);
+isEven(75);
