be12a0b7161274c65efec5141ae6c18e35e0ce27
nicktamburro <nicholastamburro@gmail.com>
2018-03-18 21:56:40 -0700

    fixed reverseArrayInPlace

--- 7-reverseArray.js
+++ 7-reverseArray.js
@@ -18,14 +18,14 @@ function reverseArrayInPlace(array){
 let placeholder = "";
 
 for (i=0 ; i<array.length / 2 ; i++){
-  placeholder = array[array.length - 1];
-  array[array.length - 1] = array[i];
+  placeholder = array[array.length - 1 - i];
+  array[array.length - 1 - i] = array[i];
   array[i] = placeholder;
-  i++;
 }
 
 console.log(array);
 
 }
 
+reverseArray(["D", "E", "L", "I", "V", "E", "R"]);
 reverseArrayInPlace(["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"]);
