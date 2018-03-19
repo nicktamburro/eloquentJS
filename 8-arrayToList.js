fa4fbd9410d19ef014593da617fe0fa0ae86b669
nicktamburro <nicholastamburro@gmail.com>
2018-03-18 22:01:17 -0700

    simple set up for arrayToList

---
+++ 8-arrayToList.js
@@ -0,0 +1,19 @@
+//this is going to be a big one
+
+function arrayToList(array){
+
+}
+
+function listToArray(list){
+
+}
+
+function prepend(element, list){
+
+}
+
+function nth(list){
+
+}
+
+arrayToList([1, 2, 3]);
