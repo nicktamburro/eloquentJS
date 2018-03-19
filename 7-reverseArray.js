9e76ce22c68efb1a264e3e6ba53d19674b113677
nicktamburro <nicholastamburro@gmail.com>
2018-03-18 21:29:25 -0700

    wrote solution for first version of reverseArray

--- 7-reverseArray.js
+++ 7-reverseArray.js
@@ -3,9 +3,9 @@ function reverseArray(array){
     let reversed = [];
 
     for (i=0 ; i< array.length ; i++){
-        reversed.unshift(array.shift(array[i]));
+        reversed.unshift(array[i]);
     }
     console.log(reversed);
 }
 
-reverseArray(["a", "b", "c", "d"]);
+reverseArray(["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"]);
