47d210ea874d605e4890f3049d1fbc88532e9741
nicktamburro <nicholastamburro@gmail.com>
2018-03-18 21:31:35 -0700

    small edits

--- 7-reverseArray.js
+++ 7-reverseArray.js
@@ -1,3 +1,6 @@
+//write a function that takes an array, and returns a new reversed array, without using the built-in "reverse" method
+//THEN write a function to reverse the array without using a new array : reverseArrayInPlace();
+
 
 function reverseArray(array){
     let reversed = [];
