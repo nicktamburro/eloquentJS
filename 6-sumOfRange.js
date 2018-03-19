74501c35d31fe0f8b5369224865ee9a5ec01d360
nicktamburro <nicholastamburro@gmail.com>
2018-03-18 21:14:05 -0700

    wrote solution, and bonus 'step' argument for sumOfRange

--- 6-sumOfRange.js
+++ 6-sumOfRange.js
@@ -1,13 +1,28 @@
 
-let rangeArray = [];
+   let rangeArray = [];
+
 
 function range(start, end, step){
-    if(start < end){
-        rangeArray.push(start);
-        start = start + 1;
-        range();
+    
+ 
+
+    for (i=start ; i<end + 1 ; i+=step){
+        rangeArray.push(i);
     }
     console.log(rangeArray);
 }
 
-range(3, 10);
+function sum(array){
+
+    let count = 0;
+
+    for (i = 0 ; i<array.length ; i++){
+        count += array[i];
+         console.log(count);
+    }
+   
+}
+
+range(1, 10, 1);
+sum(rangeArray);
+
