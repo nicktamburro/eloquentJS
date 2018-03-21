95e00cd540662b026cafb3f97d335ba3e67390be
nicktamburro <nicholastamburro@gmail.com>
2018-03-20 20:42:48 -0700

    second function for arrayToList

--- 8-arrayToList.js
+++ 8-arrayToList.js
@@ -3,8 +3,11 @@
 
 
 function arrayToList(array) {
+//starts at null, because that's what the books asked for
   let list = null;
+//backwards for loop to work from the end of the array
   for (let i = array.length - 1; i >= 0; i--) {
+      //creates each object
     list = {value: array[i], rest: list};
   }
   //return list;
@@ -12,8 +15,16 @@ function arrayToList(array) {
 }
 
 function listToArray(list){
-
+  let array = [];
+//I don't fully understand this loop yet...
+  for (i = list; i; i = i.rest) {
+    array.push(i.value);
+  }
+  console.log(array);
 }
+
+
+
 
 function prepend(element, list){
 
@@ -23,4 +34,5 @@ function nth(list){
 
 }
 
-arrayToList([1, 2, 3]);
+//arrayToList([1, 2, 3]);
+listToArray({ value: 1, rest: { value: 2, rest: { value: 3, rest: null } } });
