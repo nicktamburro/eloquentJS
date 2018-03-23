93cccabbd702e0e77e0a9cecbf81e260543142ba
nicktamburro <nicholastamburro@gmail.com>
2018-03-22 21:37:52 -0700

    started working on object comparison, deepComparison

--- 9-deepComparison.js
+++ 9-deepComparison.js
@@ -2,13 +2,23 @@
 //values are also equal when compared with a recursive call to deepEqual...
 
 function deepEqual(a, b){
+    if (typeof(a) != typeof(b)){
+        console.log("false and weird");
+    }
+    if (typeof(a) == "object" && typeof(b) == "object"){
+        console.log("objects!");
+
+    }
     if (a == b){
         console.log("true");
     }
     else {
         console.log("false");
-    }
 }
+}
+    
 
 deepEqual(2, 2);
 deepEqual(2, 3);
+deepEqual(2, "fox");
+deepEqual({}, {});
