6415e96e3e7dc38f877baae222aa9a187e08cd1b
nicktamburro <nicholastamburro@gmail.com>
2018-03-23 22:14:58 -0700

    deepComparison pretty much working, not pretty

--- 9-deepComparison.js
+++ 9-deepComparison.js
@@ -1,31 +1,25 @@
 //write deepEqual() takes 2 values, and returns true only if they're same value, or are object with same properties, whose
 //values are also equal when compared with a recursive call to deepEqual...
 
-//this will need to get way cleaned up, and make the loop work in a more general sense...
-//but it's a start
+//this is maybe working, but the code looks dumb to have so many repetitions... let's see if we can fix it tomorrow
 
 function deepEqual(a, b){
     if (typeof(a) != typeof(b)){
         
     }
     if ((typeof(a) == "object" && typeof(a) != null) && (typeof(b) == "object" && typeof(b) != null)){
-      var xA;
-      var xB;
+   
       var countA = 0;
       var countB = 0;
       var stringA = "";
       var stringB = "";
         for (x in a) {
-            stringA += x + " " + a[x];
+            stringA += x + " " + a[x] + " ";
             countA++;
-            console.log(stringA);
-            console.log(countA);
     }
         for (x in b) {
-            stringB += x + " " + b[x];
+            stringB += x + " " + b[x] + " ";
             countB++;
-            console.log(stringB);
-            console.log(countB);
     }
     if (countA != countB){
         console.log("false, different value count");
@@ -47,4 +41,4 @@ function deepEqual(a, b){
 deepEqual(2, 2);
 deepEqual(2, 3);
 deepEqual(2, "fox");
-deepEqual({value: 1, head: "bang"}, {value: 1, head: "band", boy: "big"});
+deepEqual({value: 1, head: "band"}, {value: 1, head: "bang", boy: "band"});
